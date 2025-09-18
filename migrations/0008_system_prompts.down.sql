ec846ab42f1f6c68ba9f16fc5b592c3312024708
Mauricio S Otta <mauricio@portnumber53.com>
2025-09-17 22:42:38 -0700

    chore(migrations): keep shared trg_set_timestamp function; only drop table trigger in 0008 down

--- migrations/0008_system_prompts.down.sql
+++ migrations/0008_system_prompts.down.sql
@@ -2,7 +2,6 @@
 -- Drop per-user System Prompts storage
 
 DROP TRIGGER IF EXISTS set_timestamp_on_system_prompts ON system_prompts;
-DROP FUNCTION IF EXISTS trg_set_timestamp();
 DROP INDEX IF EXISTS ix_system_prompts_user_active;
 DROP INDEX IF EXISTS ux_system_prompts_one_default_per_user;
 DROP TABLE IF EXISTS system_prompts;
